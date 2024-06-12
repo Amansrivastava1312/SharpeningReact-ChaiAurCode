cbd506dbf23915e961f3d136d2b69b559bfd4569
Aman Srivastava <amanrajasthan123@gmail.com>
2024-06-12 23:41:01 +0530

    reactRouter (learn how to create router )

---
+++ reactRouter/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+import "./App.css";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Header />
+      {/* <Outlet /> */}
+      <Footer />
+    </>
+  );
+}
+
+export default App;
